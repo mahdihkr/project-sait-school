b667c9736f92bb2f4ed6b4b131a0a5ca3a228699
mahdi hkr <mahdiabdolgholami2@gmail.com>
2024-11-16 22:41:53 +0330

    version3

---
+++ index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<td id="mahdi"colspan="7">
+ <img src="asal.jpg">
+  </td>
+  <p>عسل </p>
+</body>
+</html>
